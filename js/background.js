64f9a55ea1964e0a42019a50a15812ee78422bad
taiki <turkey.31452@gmail.com>
2022-08-30 23:24:40 +0900

    Support for firefox

--- js/background.js
+++ js/background.js
@@ -1,11 +1,15 @@
 const githubUrlRegex = /https?:\/\/github\.com\/.*\/network\/members/;
-chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+if (typeof browser === "undefined") {
+    var browser = chrome;
+}
+
+browser.tabs.onUpdated.addListener(function (tabId, info, tab) {
 	if (
 		info.status === "complete" &&
 		tab.title.indexOf("Fork") !== -1 &&
 		githubUrlRegex.test(tab.url)
 	) {
-		chrome.scripting.executeScript({
+		browser.scripting.executeScript({
 			target: { tabId: tabId },
 			files: ["js/main.js"],
 		});
