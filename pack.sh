1bb60852cfbe2abea6915c9dbe97b49ce8cf85a9
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-12-22 12:22:50 +0100

    build: bump version to 1.2.3

--- pack.sh
+++ pack.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-VERSION="1.2.1-chrome"
+VERSION="1.2.3-chrome"
 ICONS_SIZES="16 32 48 64 128"
 OUTPUT_FOLDER=".build"
 
 # Prepare icons
 for size in ${ICONS_SIZES}; do
     (
-    cd icons || exit 1
-    inkscape -w "${size}" -h "${size}" source.svg --export-filename "icon.${size}.png"
+        cd icons || exit 1
+        inkscape -w "${size}" -h "${size}" source.svg --export-filename "icon.${size}.png"
     )
 done
 
 # Pack to zip
 mkdir "${OUTPUT_FOLDER}"
-7z a "${OUTPUT_FOLDER}/${VERSION}.zip" -x!pack.sh -x!icons/source.svg -x!.git -x!.gitignore -x!.ignoreme -x!"${OUTPUT_FOLDER}"
+7z a "${OUTPUT_FOLDER}/${VERSION}.zip" -x!pack.sh -x!icons/source.svg -x!.git -x!.gitignore -x!.ignoreme -x!.github -x!"${OUTPUT_FOLDER}"
